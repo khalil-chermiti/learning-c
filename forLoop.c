736a5f413bc25905a45916caae6c98e4b83d821b
khalil chermiti <khalil666chermiti@gmail.com>
2021-10-10 01:59:13 +0100

    for loop in c

---
+++ forLoop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// this is the for loop structure in c ;
+int main() {
+
+    for (int i = 1; i <= 10 ; i++) {
+        printf("this is the iteration number : %i \n", i );
+    }
+
+}
