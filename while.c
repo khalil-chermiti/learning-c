c56f7659fb257c3b977d27a56278d650bb72e4a8
khalil chermiti <khalil666chermiti@gmail.com>
2021-10-10 01:59:57 +0100

    while loop in c

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// the structure of while statement in c
+
+int main () {
+
+    int a = 0 ;
+
+    while (a <= 10 ) {
+        printf("counting... %i\n" , a) ;
+        a++ ;
+    }
+}
