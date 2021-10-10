99260cb3db535e68dede2ff2ff8290fe99ce4001
khalil chermiti <khalil666chermiti@gmail.com>
2021-10-10 02:00:12 +0100

    do while loop in c

---
+++ doWhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// this is the do while structure in c
+int main () {
+
+    int a = 1 ;
+
+    do {
+
+        printf("this is iteration number : %i \n" , a) ;
+        a++ ;
+
+    } while( a <= 10 ) ;
+}
